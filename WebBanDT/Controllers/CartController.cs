b9a969b782eff69792d40ae4fe3e093106b13f8f
qangnh <143860977+qangnh@users.noreply.github.com>
2025-10-05 21:54:08 +0700

    Add project files.

---
+++ WebBanDT/Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanDT.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult GioHang()
+        {
+            return View();
+        }
+        public ActionResult GiaoHang()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebBanDT/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanDT.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult SanPham()
+        {
+            return View();
+        }
+    }
+}
