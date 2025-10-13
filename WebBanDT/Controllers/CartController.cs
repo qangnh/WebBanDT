e96674b4afe48c983a7ce696f768e914a424a8af
TrungDat <trungddat2004@gmail.com>
2025-10-14 00:39:15 +0700

    update code

--- WebBanDT/Controllers/CartController.cs
+++ WebBanDT/Controllers/CartController.cs
@@ -3,27 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebBanDT.Models;
 
 namespace WebBanDT.Controllers
 {
     public class CartController : Controller
     {
-        // GET: Cart
         public ActionResult GioHang()
         {
             return View();
         }
+
         public ActionResult GiaoHang()
         {
             return View();
         }
+
+        // GET: ThongTin
         public ActionResult ThongTin()
         {
-            return View();
+            var model = Session["OrderInfo"] as OrderInfoViewModel ?? new OrderInfoViewModel();
+            return View(model);
         }
+
+        // POST: ThongTin
+        [HttpPost]
+        public ActionResult ThongTin(OrderInfoViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Session["OrderInfo"] = model; // lưu dữ liệu vào session
+                return RedirectToAction("ThanhToan");
+            }
+            return View(model);
+        }
+
+        // GET: ThanhToan
         public ActionResult ThanhToan()
         {
-            return View();
+            var model = Session["OrderInfo"] as OrderInfoViewModel ?? new OrderInfoViewModel();
+            return View(model);
         }
     }
 }
---
+++ WebBanDT/Models/OrderInfoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanDT.Models
+{
+    public class OrderInfoViewModel
+    {
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public string Province { get; set; }
+        public string District { get; set; }
+        public string Ward { get; set; }
+        public string Note { get; set; }
+        public string DeliveryMethod { get; set; }
+        public bool ChinhSach { get; set; }
+    }
+}
